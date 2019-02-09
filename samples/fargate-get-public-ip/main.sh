87ccad50cdf77cc25ac535e07b868aa8287c525b
Pahud <pahudnet@gmail.com>
2019-02-09 04:23:31 +0000

    fix #10 - support cloudmap API to update Route53 RR

--- samples/fargate-get-public-ip/main.sh
+++ samples/fargate-get-public-ip/main.sh
@@ -5,6 +5,21 @@
 # check https://twitter.com/pahudnet/status/1080491306420363264 for screenshot
 
 
+# update with cloudmap
+cm_register_instance() {
+    # Usage: cm_register_instance serviceId instanceId ipAddress
+    aws servicediscovery register-instance --service-id $1 \
+    --instance-id "$2" \
+    --attributes=AWS_INSTANCE_IPV4="$3"
+}
+
+cm_deregister_instance() {
+    # Usage: cm_deregister_instance serviceId instanceId 
+    aws servicediscovery deregister-instance --service-id $1 --instance-id "$2" 
+}
+
+
+
 echo "====[EVENT]===="
 echo "$1"
 echo "====[/EVENT]===="
@@ -13,7 +28,7 @@ region=${AWS_REGION}
 
 clusterArn=$(echo $1 | jq -r '.detail.clusterArn')
 
-taskArn=$(echo "$1"  | \
+taskArnRunning=$(echo "$1"  | \
 jq -r 'select(
     .["detail-type"]=="ECS Task State Change" and
     .source=="aws.ecs" and
@@ -21,39 +36,36 @@ jq -r 'select(
     .detail.desiredStatus=="RUNNING" and 
     .detail.connectivity=="CONNECTED"
     )  | .detail.taskArn')
+
+taskArnStopped=$(echo "$1"  | \
+jq -r 'select(
+    .["detail-type"]=="ECS Task State Change" and
+    .source=="aws.ecs" and
+    .detail.lastStatus=="STOPPED" and 
+    .detail.desiredStatus=="STOPPED" and 
+    .detail.connectivity=="CONNECTED"
+    )  | .detail.taskArn')
     
-if [ -z $taskArn ]; then
+if [[ -z $taskArnRunning && -z  $taskArnStopped ]]; then
     echo "[INFO] skipping this event"
     exit 0
 fi
 
-eni=$(aws --region $region ecs describe-tasks --tasks $taskArn --cluster $clusterArn | \
-jq -r '.tasks[0].attachments[0].details[] | select(.name=="networkInterfaceId").value')
-
-publicIp=$(aws --region $region ec2 describe-network-interfaces --network-interface-ids $eni | \
-jq -r '.NetworkInterfaces[0].Association.PublicIp')
-
-echo "=> [OK] new Amazon ECS task stablized"
-echo "taskArn=$taskArn"
-echo "eni=$eni"
-echo "publicIp=$publicIp"
-
-
-# update route53
-cat << EOF > /tmp/update.json
-{
-    "Comment": "UPSERT a record ",
-    "Changes": [{
-    "Action": "UPSERT",
-    "ResourceRecordSet": {
-        "Name": "fargate.demo.pahud.net",
-        "Type": "A",
-        "TTL": 300,
-        "ResourceRecords": [{ "Value": "$publicIp"}]
-}}]
-}
-EOF
-
-aws route53 change-resource-record-sets --hosted-zone-id Z2N5MJJUEIAVLZ --change-batch file:///tmp/update.json
+if [ ! -z $taskArnRunning ]; then
+    eni=$(aws --region $region ecs describe-tasks --tasks $taskArnRunning --cluster $clusterArn | \
+    jq -r '.tasks[0].attachments[0].details[] | select(.name=="networkInterfaceId").value')
+    
+    publicIp=$(aws --region $region ec2 describe-network-interfaces --network-interface-ids $eni | \
+    jq -r '.NetworkInterfaces[0].Association.PublicIp')
+    echo "=> [OK] new Amazon ECS task stablized"
+    echo "taskArn=$taskArnRunning"
+    echo "eni=$eni"
+    echo "publicIp=$publicIp"
+    echo "new task running - register cloudmap now"
+    cm_register_instance 'srv-emzgomv5qbxsqtd4' "${taskArnRunning##*/}" "$publicIp"
+elif [ ! -z $taskArnStopped ]; then
+    echo "task stopped - deregister cloudmap now"
+    cm_deregister_instance 'srv-emzgomv5qbxsqtd4' "${taskArnStopped##*/}"
+fi
 
 exit 0
