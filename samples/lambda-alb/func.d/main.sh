d05df41894f9d8ab1458dff526509386d0567c12
Pahud Hsieh <pahudnet@gmail.com>
2019-05-19 16:13:09 +0000

    minor

--- samples/lambda-alb/func.d/main.sh
+++ samples/lambda-alb/func.d/main.sh
@@ -2,10 +2,8 @@
 
 export PATH=$PATH:/opt/awscli
 
-event_json_dump=$(echo "$1" | jq '.|@json')
+event=$(echo "$1" | jq '.|@json')
 
-
-# echo the http response for API Gateway proxy integration
 cat << EOF
-{"body": ${event_json_dump}, "headers": {"content-type": "application/json"}, "statusCode": 200}
+{"body": ${event}, "headers": {"content-type": "application/json"}, "statusCode": 200}
 EOF
